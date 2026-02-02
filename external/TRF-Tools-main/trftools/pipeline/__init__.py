d8a899d204dbd5fdf246159be767c4486534c42c
xiaoshao <shaox18@mcmaster.ca>
2026-02-01 22:54:30 -0500

    delete TRF package, use python package

--- external/TRF-Tools-main/trftools/pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
-from ._code import Code
-from ._experiment import TRFExperiment
-from ._model import Term
-from ._predictor import EventPredictor, FilePredictor, MakePredictor, SessionPredictor
-from ._results import ResultCollection
--- external/TRF-Tools-main/trftools/roi/__init__.py
+++
@@ -1,4 +0,0 @@
-# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
-"""Tools for ROI analysis"""
-from ._roi import mask_roi, mne_label
-from ._froi import symmetric_mask
